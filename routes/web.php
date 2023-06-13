412071afce6520a2d2784f27103f7d820f522ca0
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-06-13 22:23:52 +0400

    Redirect from login then already logged in

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
-});
+})->middleware('guest');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
