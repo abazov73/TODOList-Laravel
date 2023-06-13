a4844c6c2a8ab063df6691c033cf96d0d0bcc907
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-06-13 19:49:25 +0400

    Task controller + task view

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $tasks = $request->user()->tasks()->get();
+ 
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+    
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+    
+        return redirect('/tasks');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/task', [TaskController::class, 'store']);
+Route::delete('/task/{task}', [TaskController::class, 'destroy']);
